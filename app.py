d7c2537b0d6e0dcfc5d867d02ec2e2b68e1c81fd
Osanmaz <osanmaz4992@gmail.com>
2020-03-30 20:25:43 +0100

    clean coding

--- app.py
+++ app.py
@@ -1,37 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://m@localhost:5432/todosapp' ##db login
+# db login
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://m@localhost:5432/todosapp'
 db = SQLAlchemy(app)
 
 
-##database model
+# database model
 class Todo(db.Model):
     __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key=True) ##id of db's columns
-    description = db.Column(db.String(), nullable=False) ##data itself
+    id = db.Column(db.Integer, primary_key=True)  # id of db's columns
+    description = db.Column(db.String(), nullable=False)  # data itself
 
-    def __repr__(self): ##debugging repr method
+    def __repr__(self):  # debugging repr method
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all() ##
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        # request.get.json body json response
+        description = request.get_json()['description']
+        todo = Todo(description=description)  # db update
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.ext_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)
 
 
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all()) ##include data from data base
 
-
-@app.route('/todos/create', methods=['POST'])
-def create_todo():
-  description = request.form.get('description', '') ##request.form.get grabs form
-  todo = Todo(description=description) ##db update
-  db.session.add(todo)
-  db.session.commit()
-  return redirect(url_for('index')) ##redirect
-
-
-if(__name__)== '__main__': 
-    app.run()
